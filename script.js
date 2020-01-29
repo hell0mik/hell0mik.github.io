273cbebe4aea6d4d565b014765b39f350ddbadc0
hell0mik <60320912+hell0mik@users.noreply.github.com>
2020-01-29 16:33:27 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var fileref=document.createElement("link");
+fileref.setAttribute("rel", "stylesheet");
+fileref.setAttribute("type", "text/css");
+fileref.setAttribute("href", 'https://hell0mik.github.io/style.css');
